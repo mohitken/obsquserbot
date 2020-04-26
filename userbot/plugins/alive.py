e97114bfb05e97e328c1d58a3a7355721fa66d56
obsq <63944348+obsq@users.noreply.github.com>
2020-04-26 21:23:25 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -16,8 +16,8 @@ async def amireallyalive(alive):
     await alive.edit("`░█─░█ █▀▀ █── █── █▀▀█ `"
 "                     `░█▀▀█ █▀▀ █── █── █──█ `"
 "                     `░█─░█ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀▀ `\n\n`⋙⋙Telethon version:6.9.0`\n`⋙⋙Python version:3.7.1`\n`⋙⋙Bot version:1.1.2`\n\n"
-                     f"▒▒`My owner`: {DEFAULTUSER}\n▒▒`channel`:@crackedapps_obsq\n"
-                     "`▒▒Bot name:obsqofficial_bot`\n\n`➳➳fork by:`@obsquriel\n\n"
+                     f"▓▓`My owner`: {DEFAULTUSER}\n▒▒`channel`:@crackedapps_obsq\n"
+                     "░░`Bot name:obsqofficial_bot`\n\n`➳➳fork by:`@obsquriel\n\n"
                      "➳➳`Database Status: Databases functioning normally!\n`\n"
                      "[DEPLOY THIS USERBOT NOW](https://github.com/obsq/obsquserbot.git)")
                      
